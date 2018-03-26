e3394f750d11eb2486ce81818babc13c1b1de39a
ayushgupta2959 <ayushgupta2959@gmail.com>
2018-03-26 10:20:26 +0530

    Test case added and code annoted with CheckerFramework index

---
+++ AverageSentenceLength/src/AnnotedCode/Main.java
@@ -0,0 +1,33 @@
+import java.util.HashSet;
+import org.checkerframework.checker.index.qual.*;
+import org.checkerframework.common.value.qual.*;
+import org.checkerframework.checker.nullness.qual.*;
+@SuppressWarnings("assignment.type.incompatible")
+public class Main {
+
+	public static void main(String @MinLen(1) [] args) {
+		@LTLengthOf("args") int i = 0;
+		String file = args [i++];
+		String wordLength = null;
+		HashSet<String> delimiters = new HashSet<>();
+		for(int j = 0;j<2;j++) {
+			if( i == args.length) {
+				//do nothing
+			}
+			else if(args[i].equals("-l")) {
+				i++;
+				wordLength = args[i++];
+			}
+			else if(args[i].equals("-d")){
+				i++;
+				while((i < args.length) && !(args[i].equals("-l")) && !(args[i].equals("-i"))) {
+					delimiters.add(args[i++]);
+				}
+			}
+			else {
+				;
+			}
+		}
+		System.out.printf("%.2f",Validate.validateInput(file,wordLength,delimiters));
+		}
+	}
---
+++ AverageSentenceLength/src/AnnotedCode/Validate.java
@@ -0,0 +1,49 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.HashSet;
+
+import org.checkerframework.checker.index.qual.*;
+import org.checkerframework.common.value.qual.*;
+import org.checkerframework.checker.nullness.qual.*;
+
+public class Validate {
+	private final static int defaultWordLength = 3;
+	private final static String[] defaultDelimiters = {".","!","?"};
+	
+	
+	
+	public static double validateInput(String file,String receivedWordLength,HashSet<String> receivedDelimiters) {
+		FileInputStream fin;
+		int wordLength;
+		if(file.endsWith(".txt")){
+			try {
+				fin = new FileInputStream(file);
+			} 
+			catch(FileNotFoundException e) {
+				System.out.println("File Not Found\n");
+				return -1;
+			}
+		}
+		else {
+			System.out.println("File is not in .txt format\n");
+			return -1;
+		}
+		
+		if(receivedWordLength != null) {
+			try {
+				wordLength = Integer.parseInt(receivedWordLength);
+			}
+			catch(NumberFormatException e){
+				System.out.println("Word Length should be an integer");
+				return -1;
+			}
+		}
+		else {
+			wordLength = defaultWordLength;
+		}
+		for(String s : defaultDelimiters) {
+			receivedDelimiters.add(s);
+		}
+		return InformationProcessor.processor(fin,wordLength,receivedDelimiters);
+	}
+}
